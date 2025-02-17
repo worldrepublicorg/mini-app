a5cf9a1d51eeab48786425566eba1f0ec3166e44
blaisekonya <blaisekonya@gmail.com>
2025-02-17 13:42:24 +0100

    Update Worldcoin swap referrer app ID in earn page

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -335,7 +335,7 @@ export default function EarnPage() {
         </div>
         {checkWalletAuth() && (
           <a
-            href="https://worldcoin.org/mini-app?app_id=app_a4f7f3e62c1de0b9490a5260cb390b56&path=%3Ftab%3Dswap%26fromToken%3D0x2cFc85d8E48F8EAB294be644d9E25C3030863003%26amount%3D1000000000000000000%26toToken%3D0xEdE54d9c024ee80C85ec0a75eD2d8774c7Fbac9B%26referrerAppId%3Dapp_66c83ab8c851fb1e54b1b1b62c6ce39d"
+            href="https://worldcoin.org/mini-app?app_id=app_a4f7f3e62c1de0b9490a5260cb390b56&path=%3Ftab%3Dswap%26fromToken%3D0x2cFc85d8E48F8EAB294be644d9E25C3030863003%26amount%3D1000000000000000000%26toToken%3D0xEdE54d9c024ee80C85ec0a75eD2d8774c7Fbac9B%26referrerAppId%3Dapp_880aceacfcfbc104c4702143603579ab"
             className="flex h-10 items-center gap-2 rounded-full bg-gray-100 px-4"
           >
             <PiWalletFill className="h-5 w-5" />
