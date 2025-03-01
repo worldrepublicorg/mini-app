1a92ffc29560b7c8ee35c4391ee9af9295b9c4a3
blaisekonya <blaisekonya@gmail.com>
2025-03-01 07:33:48 +0100

    Update Collect button text to display zero

--- components/StakeWithPermitForm.tsx
+++ components/StakeWithPermitForm.tsx
@@ -426,7 +426,7 @@ export function StakeWithPermitForm() {
             size="sm"
             className="mr-2 h-9 w-20 rounded-full px-4 font-sans"
           >
-            Collect
+            Collect 0
           </Button>
           <Typography
             variant={{ variant: "number", level: 6 }}
