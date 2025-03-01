79f7d82ba9e317819696e5196d21c97bcd4b7b93
blaisekonya <blaisekonya@gmail.com>
2025-03-01 03:47:07 +0100

    Update Earn page text and loading state

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -226,7 +226,7 @@ export default function EarnPage() {
                   level={1}
                   className="mx-auto mb-10 mt-4 text-center text-gray-500"
                 >
-                  Claimable drachma
+                  Claimable drachma test
                 </Typography>
                 <div className="text-center">
                   <p className="mx-auto mb-14 font-sans text-[56px] font-semibold leading-narrow tracking-normal">
@@ -235,7 +235,7 @@ export default function EarnPage() {
                 </div>
                 <Button
                   onClick={sendClaim}
-                  isLoading={isSubmitting || isConfirming}
+                  isLoading={isSubmitting}
                   fullWidth
                 >
                   Claim
