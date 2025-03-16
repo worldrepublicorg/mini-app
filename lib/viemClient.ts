485f93d95904148557c8fcc609e9c2a27d517ce5
blaisekonya <blaisekonya@gmail.com>
2025-03-16 05:46:02 +0100

    Enhance viemClient configuration by implementing a fallback transport mechanism with multiple HTTP and WebSocket endpoints for improved reliability and performance.

--- lib/viemClient.ts
+++ lib/viemClient.ts
@@ -1,7 +1,28 @@
-import { createPublicClient, http } from "viem";
+import { createPublicClient, http, fallback, webSocket } from "viem";
 import { worldchain } from "viem/chains";
+
+const optimizedFallbackConfig = {
+  retryCount: 10,
+  retryDelay: 100,
+};
 
 export const viemClient = createPublicClient({
   chain: worldchain,
-  transport: http("https://worldchain-mainnet.g.alchemy.com/v2/_NsuCc68eeT4D5AZNEGdDG5nms3v8gcY"),
+  transport: fallback(
+    [
+      http(
+        "https://lb.drpc.org/ogrpc?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdQFo2ndAh8R8ICcfhHoK236"
+      ),
+      webSocket(
+        "wss://lb.drpc.org/ogws?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdQFo2ndAh8R8ICcfhHoK236"
+      ),
+      http(
+        "https://lb.drpc.org/ogrpc?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdRm99dUAfUR8ICJfhHoK236"
+      ),
+      webSocket(
+        "wss://lb.drpc.org/ogws?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdRm99dUAfUR8ICJfhHoK236"
+      ),
+    ],
+    optimizedFallbackConfig
+  ),
 }) as any;
