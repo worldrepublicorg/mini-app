2a9737830f86d7ed23de10d66099943550c84b94
blaisekonya <blaisekonya@gmail.com>
2025-03-16 05:53:21 +0100

    Expand viemClient configuration by adding additional HTTP and WebSocket endpoints to enhance connectivity options and improve overall reliability.

--- lib/viemClient.ts
+++ lib/viemClient.ts
@@ -16,6 +16,14 @@ export const viemClient = createPublicClient({
       webSocket(
         "wss://lb.drpc.org/ogws?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdQFo2ndAh8R8ICcfhHoK236"
       ),
+      http("https://sparkling-autumn-dinghy.worldchain-mainnet.quiknode.pro"),
+      http("https://worldchain-mainnet.g.alchemy.com/public"),
+      http("https://worldchain-mainnet.gateway.tenderly.co"),
+      webSocket("wss://worldchain-mainnet.gateway.tenderly.co"),
+      http("https://480.rpc.thirdweb.com"),
+      http(
+        "https://node.histori.xyz/worldchain-mainnet/8ry9f6t9dct1se2hlagxnd9n2a"
+      ),
       http(
         "https://lb.drpc.org/ogrpc?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdRm99dUAfUR8ICJfhHoK236"
       ),
