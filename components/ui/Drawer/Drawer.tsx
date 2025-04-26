e5e5618c1a0c778f6977998fdd46decbf10012da
blaisekonya <blaisekonya@gmail.com>
2025-04-27 01:32:54 +0200

    Refactor Drawer component to accept customizable snap points and improve content overflow handling for better layout management.

--- components/ui/Drawer/Drawer.tsx
+++ components/ui/Drawer/Drawer.tsx
@@ -4,7 +4,10 @@ import { cn } from "@/lib/utils";
 import * as React from "react";
 import { Drawer as DrawerPrimitive } from "vaul";
 
-const Drawer = DrawerPrimitive.Root;
+const Drawer = ({ snapPoints = [0.1, 0.5, 0.9], ...props }) => (
+  <DrawerPrimitive.Root snapPoints={snapPoints} {...props} />
+);
+
 const DrawerTrigger: typeof DrawerPrimitive.Trigger = DrawerPrimitive.Trigger;
 const DrawerClose: typeof DrawerPrimitive.Close = DrawerPrimitive.Close;
 
@@ -14,14 +17,14 @@ const DrawerContent: typeof DrawerPrimitive.Content = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <DrawerPrimitive.Portal>
     <DrawerPrimitive.Overlay className="fixed inset-0 z-50">
-      <div className="w-full h-full bg-gray-900 opacity-40" />
+      <div className="h-full w-full bg-gray-900 opacity-40" />
     </DrawerPrimitive.Overlay>
 
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 h-auto flex flex-col rounded-t-2xl bg-gray-0 outline-none",
-        className,
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto max-h-[85vh] flex-col overflow-y-auto rounded-t-2xl bg-gray-0 outline-none",
+        className
       )}
       {...props}
     >
