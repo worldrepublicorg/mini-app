820531f8af0070fb52897c20405b58e7fb457e72
blaisekonya <blaisekonya@gmail.com>
2025-05-08 05:48:26 +0200

    Replace next.config.mjs with next.config.ts for TypeScript support and added transpilePackages configuration for mini-apps-ui-kit/react.

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  transpilePackages: ["mini-apps-ui-kit/react"],
+};
+
+export default nextConfig;
