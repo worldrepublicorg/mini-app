648ef941bea767b03be302f4429f96ae252ca79a
blaisekonya <blaisekonya@gmail.com>
2025-05-11 07:16:34 +0200

    Add new poll of the week

--- components/PollOfTheWeek.tsx
+++ components/PollOfTheWeek.tsx
@@ -23,8 +23,8 @@ const polls: Poll[] = [
     url: "https://world.org/mini-app?app_id=app_86794ef02e4fdd6579a937e4a0d858fb&app_mode=mini-app&path=/b6be3PF1",
   },
   {
-    key: "wildlifeTrafficking",
-    url: "https://world.org/mini-app?app_id=app_86794ef02e4fdd6579a937e4a0d858fb&app_mode=mini-app&path=/eWitQrSp",
+    key: "coralCrisisAction",
+    url: "https://world.org/mini-app?app_id=app_86794ef02e4fdd6579a937e4a0d858fb&app_mode=mini-app&path=/jNqEO9XV",
   },
   {
     key: "criticalMineralsExport",
