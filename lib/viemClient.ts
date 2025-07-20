87fd1ec0bd09afcfd3ecc7e4a17a4996a78c4c6e
blaisekonya <blaisekonya@gmail.com>
2025-07-20 23:40:53 +0200

    update viem client

--- lib/viemClient.ts
+++ lib/viemClient.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, http, fallback, webSocket } from "viem";
+import { createPublicClient, http, fallback } from "viem";
 import { worldchain } from "viem/chains";
 
 const optimizedFallbackConfig = {
@@ -13,22 +13,12 @@ export const viemClient = createPublicClient({
       http(
         "https://lb.drpc.org/ogrpc?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdQFo2ndAh8R8ICcfhHoK236"
       ),
-      webSocket(
-        "wss://lb.drpc.org/ogws?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdQFo2ndAh8R8ICcfhHoK236"
-      ),
       http("https://sparkling-autumn-dinghy.worldchain-mainnet.quiknode.pro"),
       http("https://worldchain-mainnet.g.alchemy.com/public"),
       http("https://worldchain-mainnet.gateway.tenderly.co"),
-      webSocket("wss://worldchain-mainnet.gateway.tenderly.co"),
       http("https://480.rpc.thirdweb.com"),
       http(
         "https://node.histori.xyz/worldchain-mainnet/8ry9f6t9dct1se2hlagxnd9n2a"
-      ),
-      http(
-        "https://lb.drpc.org/ogrpc?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdRm99dUAfUR8ICJfhHoK236"
-      ),
-      webSocket(
-        "wss://lb.drpc.org/ogws?network=worldchain&dkey=AgobUm8RhkrUlOLZyf0lrdRm99dUAfUR8ICJfhHoK236"
       ),
     ],
     optimizedFallbackConfig
